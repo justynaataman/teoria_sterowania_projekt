0acdc2d9df236c5d2fa95b8126a51187538dbf6e
Justyna <j.ataman@tlen.pl>
2020-05-10 22:23:12 +0200

    grab_obj and small changes

--- supervisor.py
+++ supervisor.py
@@ -4,7 +4,7 @@ from statemachine import StateMachine, State, Transition
 options = [
     {"name": "wait_for_xy", "initial": True, "value": "wait_for_xy"},  # 0
     {"name": "camera", "initial": False, "value": "camera"},  # 1
-    {"name": move base", "initial": False, "value": "move base"},  # 2
+    {"name": "move base", "initial": False, "value": "move base"},  # 2
     {"name": "grab_obj", "initial": False, "value": "grab_obj"},  # 3
     {"name": "move_arm", "initial": False, "value": "move_arm"}, #4
     {"name": "check_box_pos", "initial": False, "value": "check_box_pos"}, #5
@@ -85,11 +85,11 @@ class Generator(StateMachine):
        return cls(states, transitions)
  
 #best opt
-path_1 = ["m_0_1", "m_1_3", "m_3_5", "m_5_6", "m_6_7", “m_7_0”]
+path_1 = ["m_0_1", "m_1_3", "m_3_5", "m_5_6", "m_6_7", "m_7_0"]
 #worse opt
-path_2 = ["m_0_2", "m_2_1", "m_1_3", "m_3_5", “m_5_2”, “m_2_6”, “m_6_4”, “m_4_6”, “m_6_7”, “m_7_0”]
+path_2 = ["m_0_2", "m_2_1", "m_1_3", "m_3_5", "m_5_2”, "m_2_6", "m_6_4", "m_4_6", "m_6_7", "m_7_0"]
 #
-path_3 = ["m_0_1", "m_1_4", "m_4_1", "m_1_3", “m_3_5”, “m_5_6”, “m_6_4”, “m_4_6”, “m_6_7”, “m_7_0”]
+path_3 = ["m_0_1", "m_1_4", "m_4_1", "m_1_3", "m_3_5", "m_5_6", "m_6_4", "m_4_6", "m_6_7", "m_7_0"]
 paths = [path_1, path_2, path_3]
  
 for path in paths:
