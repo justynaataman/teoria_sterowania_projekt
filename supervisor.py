2fd95c3c15b5676db1163b2e305ce003430131fa
szymon.zmuds <szymon.zmudax@gmail.com>
2020-05-14 22:38:40 +0200

    adding slave's automata

--- supervisor.py
+++ supervisor.py
@@ -27,32 +27,64 @@ for path in paths:
        events = ['camera', 'move_base', 'grab_obj', 'move_arm', 'check_box_pos', 'arm_box', 'put_obj']
        print(supervisor.current_state)
        val = supervisor.current_state.value
+       slave_states = None
+       slave_transition = None
+       slave_path = None
        #for ev in events:
-
            # add slave
        if val == "camera":
-          print("cam")
-
+           print("cam")
+           slave_states = camera_states
+           slave_transition = camera_transitions
+           slave_path = paths_camera
        if val == "move_base":
            #TODO: automata 2 (for) slave2
+           slave_states = move_base_states
+           slave_transition = move_base_transitions
+           slave_path = paths_move_base
            print("move base")
-
        if val == "grab_obj":
                # TODO: automata 3 (for) slave3
            print("grab obj")
-
+           slave_states = grab_obj_states
+           slave_path = paths_grab_obj
+           slave_transition = grab_obj_transitions
        if val == "move_arm":
-               # TODO: automata 3 (for) slave3
+            # TODO: automata 3 (for) slave3
+            slave_transition = move_arm_transitions
+            slave_states = move_arm_states
+            slave_path = paths_move_arm
             print("move arm")
        if val == 'check_box_pos':
                # TODO: automata 3 (for) slave3
+            slave_transition = check_box_pose_transitions
+            slave_states = check_box_pose_states
+            slave_path = paths_check_box_pos
             print("check box pos")
        if val == "arm_box":
                # TODO: automata 3 (for) slave3
+            slave_transition = arm_box_transitions
+            slave_states = arm_box_states
+            slave_path = paths_arm_box
             print("arm box")
        if val == "put_obj":
-               # TODO: automata 3 (for) slave3
-            print("put obj")
+            # TODO: automata 3 (for) slave3
+            slave_transition = put_obj_transitions
+            slave_states = put_obj_states
+            slave_path = paths_put_obj
+            print('put obj')
+       #create slave automata
+       print('automata')
+       if slave_states is not None:
+           slave = Generator.create_master(slave_states, slave_transition)
+           #for now first path
+           ss = slave_path[0]
+           for x in ss:
+               slave_transition[x]._run(slave)
+               print(slave.current_state)
+
+
+
 
  
 print('Supervisor done!')
